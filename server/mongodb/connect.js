1b447ea07cbea6f0896c7afb4d49cc7fea4bc8de
Ashim Ghimire <aghimire85@gmail.com>
2023-01-25 17:01:03 -0500

    Backend basic server created

---
+++ server/mongodb/connect.js
@@ -0,0 +1,13 @@
+import { urlencoded } from "express";
+import mongoose from "mongoose";
+
+const connectDB = (url) => {
+  mongoose.set("strictQuery", true);
+
+  mongoose
+    .connect(url)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.error(err));
+};
+
+export default connectDB;
